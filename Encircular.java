dc2f62eb1a1e3a8f8ba9a0f353865c86f7fc5833
Michael <kmichael24@gmail.com>
2015-10-08 10:57:39 -0400

    updated

--- Encircular.java
+++ Encircular.java
@@ -10,59 +10,69 @@ public class Encircular {
 	// and direction="N","E","S","W"
 	static String doesCircleExist(String commands) {
 
-		int initialX = 0;
-		int initialY = 0;
+		int x1 = 0;
+		int y1 = 0;
 		
-		int x = 0;
-		int y = 0;
-		String direction = "north";
+		int x2 = 0;
+		int y2 = 0;
+		String direction = "N";
 		
 		for (int i = 0; i < commands.length(); i++) {
-			
-			if (direction.equals("north")) {
-				if (commands.charAt(i) == 'G') {
-					y++;
-				} else if (commands.charAt(i) == 'L') {
-					direction = "west";
-				} else if (commands.charAt(i) == 'R') {
-					direction = "east";
-				} else {
-					System.out.println("Wrong command");
+			switch (direction){
+				case "N":
+				if (commands.charAt(i) == 'G'){
+					y2++;
 				}
-			} else if (direction.equals("east")) {
-				if (commands.charAt(i) == 'G') {
-					x++;
-				} else if (commands.charAt(i) == 'L') {
-					direction = "north";
-				} else if (commands.charAt(i) == 'R') {
-					direction = "south";
-				} else {
-					System.out.println("Wrong command");
+				else if (commands.charAt(i)	=='L'){
+					direction = "W";
 				}
-			} else if (direction.equals("south")) {
-				if (commands.charAt(i) == 'G') {
-					y--;
-				} else if (commands.charAt(i) == 'L') {
-					direction = "east";
-				} else if (commands.charAt(i) == 'R') {
-					direction = "west";
-				} else {
-					System.out.println("Wrong command");
+				else if (commands.charAt(i) == 'R'){
+					direction = "E";
 				}
-			} else if (direction.equals("west")) {
-				if (commands.charAt(i) == 'G') {
-					x--;
-				} else if (commands.charAt(i) == 'L') {
-					direction = "south";
-				} else if (commands.charAt(i) == 'R') {
-					direction = "north";
-				} else {
-					System.out.println("Wrong command");
+				break;
+				
+				case "E":
+				if (commands.charAt(i) == 'G'){
+					x2++;
 				}
+				else if (commands.charAt(i)	=='L'){
+					direction = "N";
+				}
+				else if (commands.charAt(i) == 'R'){
+					direction = "S";
+				}
+				break;
+				
+				case "S":
+				if (commands.charAt(i) == 'G'){
+					y2--;
+				}
+				else if (commands.charAt(i)	=='L'){
+					direction = "E";
+				}
+				else if (commands.charAt(i) == 'R'){
+					direction = "W";
+				}
+				break;
+				
+				case "W":
+				if (commands.charAt(i) == 'G'){
+					x2--;
+				}
+				else if (commands.charAt(i)	=='L'){
+					direction = "S";
+				}
+				else if (commands.charAt(i) == 'R'){
+					direction = "N";
+				}
+				break;
+				default:
+				System.out.println("Invalid input");
+				break;
 			}
 		}
 		
-		if (direction.equals("north") && (((x-initialX)*(x-initialX) + (y-initialY)*(y-initialY)) > 0)) {
+		if (direction.equals("N") && ((Math.pow(x2-x1,2) + Math.pow(y2-y1,2)) > 0)) {
 			return "NO";
 		} else {
 			return "YES";
