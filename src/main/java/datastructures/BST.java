11683f0697d272c2e19664bd37c6c7c4a30b1b1c
Michael <kmichael24@gmail.com>
2015-10-28 16:05:43 -0400

    line change

--- src/main/java/datastructures/BST.java
+++ src/main/java/datastructures/BST.java
@@ -39,7 +39,7 @@ public class BST<Key extends Comparable<Key>, Value>
 		if (x == null) return null;
 		int cmp = key.compareTo(x.key);
 		if 		(cmp < 0) 	return get(x.left, key);
-		else if (cmp > 0) 	return get(x.right, key);
+		else if (cmp > 0) 		return get(x.right, key);
 		else return x.val;
 	}
 	
