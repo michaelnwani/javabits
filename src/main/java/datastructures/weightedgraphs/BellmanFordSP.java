0fe79be80d4698d85211650ac26ba3e90da93884
Michael <kmichael24@gmail.com>
2015-11-13 12:52:59 -0500

    Implemented Critical Path method, Bellman-Ford algorithms

---
+++ src/main/java/datastructures/weightedgraphs/BellmanFordSP.java
@@ -0,0 +1,96 @@
+// Bellman-Ford algorithm (queue-based)
+// worst case time proportional to EV, space prop. to V
+// works with negative weights and cycles
+public class BellmanFordSP {
+	private double[] distTo;				// length of path to v
+	private DirectedEdge[] edgeTo;			// last edge on path to v
+	private boolean[] onQ;					// Is this vertex on the queue?
+	private Queue<Integer> queue;			// vertices being relaxed
+	private int cost;						// number of calls to relax()
+	private Iterable<DirectedEdge> cycle;	// negative cycle in edgeTo[]?
+
+	public BellmanFordSP(EdgeWeightedDigraph G, int s) {
+		distTo = new double[G.V()];
+		edgeTo = new DirectedEdge[G.V()];
+		onQ = new boolean[G.V()];
+		queue = new Queue<Integer>();
+		for (int v = 0; v < G.V(); v++) {
+			distTo[v] = Double.POSITIVE_INFINITY;
+		}
+		distTo[s] = 0.0;
+		queue.enqueue(s);
+		onQ[s] = true;
+		while (!queue.isEmpty() && !hasNegativeCycle()) {
+			int v = queue.dequeue();
+			onQ[v] = false;
+			relax(G, v);
+		}
+	}
+	// Relaxation for Bellman-Ford
+	private void relax(EdgeWeightedDigraph G, int v) {
+		for (DirectedEdge e : G.adj(v)) {
+			int w = e.to();
+			if (distTo[w] > distTo[v] + e.weight()) {
+				distTo[w] = distTo[v] + e.weight();
+				edgeTo[w] = e;
+				if (!onQ[w]) {
+					queue.enqueue(w);
+					onQ[w] = true;
+				}
+			}
+			if (cost++ % G.V() == 0) {
+				findNegativeCycle();
+			}
+		}
+	}
+
+	// standard client query methods
+	// for SPT implementations
+	public double distTo(int v) {
+		return distTo[v];
+	}
+
+	public boolean hasPathTo(int v) {
+		return distTo[v] < Double.POSITIVE_INFINITY;
+	}
+
+	public Iterable<DirectedEdge> pathTo(int v) {
+		if (!hasPathTo(v)) {
+			return null;
+		}
+
+		Stack<DirectedEdge> path = new Stack<DirectedEdge>();
+		for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+			path.push(e);
+		}
+		return path;
+	}
+
+	private void findNegativeCycle() {
+		int V = edgeTo.length;
+		EdgeWeightedDigraph spt = new EdgeWeightedDigraph(V);
+		for (int v = 0; v < V; v++) {
+			if (edgeTo[v] != null) {
+				spt.addEdge(edgeTo[v]);
+			}
+		}
+
+		EdgeWeightedDirectedCycle cf = new EdgeWeightedDirectedCycle(spt);
+
+		cycle = cf.cycle();
+	}
+
+	public boolean hasNegativeCycle() {
+		return cycle != null;
+	}
+
+	public Iterable<DirectedEdge> negativeCycle() {
+		return cycle;
+
+	}
+}
+
+
+
+
+
